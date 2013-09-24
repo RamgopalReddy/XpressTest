b0e60542a866f7d6df09e6cef7772ea44446c81a
RamgopalReddy <RamgopalReddy@sats-Mac-mini.local>
2013-09-24 14:38:21 +0530

    Initial Commit

---
+++ XpressTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XpressTest
+//
+//  Created by Ramgopal Reddy on 9/24/13.
+//  Copyright (c) 2013 SYNFOSYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XpressTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XpressTest
+//
+//  Created by Ramgopal Reddy on 9/24/13.
+//  Copyright (c) 2013 SYNFOSYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
